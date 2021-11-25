bb86444f2c5a19e45fadf32d42b0c596de2573d3
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-11-25 18:55:33 +0900

    環境構築

---
+++ docker/php/laravel-install.sh
@@ -0,0 +1,19 @@
+echo "<-- start install laravel -->"
+composer create-project --prefer-dist laravel/laravel . "8.*" --prefer-dist
+echo "<-- finish install laravel -->"
+php artisan -V
+
+# Laravelのデバッガーツール
+echo "<-- start install debugbar -->"
+composer require barryvdh/laravel-debugbar
+echo "<-- finish install debugbar -->"
+
+# Laravel UIツール
+echo "<-- start install Laravel Breeze -->"
+composer require laravel/breeze "1.*" --dev
+php artisan breeze:install
+echo "<-- finish install Laravel Breeze -->"
+
+echo "<-- start install npm -->"
+npm install && npm run dev
+echo "<-- finish install npm -->"
