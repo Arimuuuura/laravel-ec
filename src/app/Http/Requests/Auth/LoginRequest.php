225f7b74bd5fc417b57ba6f18e3677a546728356
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-11-28 02:21:06 +0900

    マルチログイン機能

--- src/app/Http/Requests/Auth/LoginRequest.php
+++ src/app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,16 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        // config/auth.php で設定している値
+        if ($this->routeIs('admin.*')) {
+            $guard = 'admin';
+        } elseif ($this->routeIs('owner.*')) {
+            $guard = 'owners';
+        } else {
+            $guard = 'users';
+        }
+
+        if (! Auth::guard($guard)->attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
