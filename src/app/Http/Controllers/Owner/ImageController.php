a94fcc0521b6b78968d7b65d44232a68167df434
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-12 14:50:44 +0900

    images delete

--- src/app/Http/Controllers/Owner/ImageController.php
+++ src/app/Http/Controllers/Owner/ImageController.php
@@ -8,6 +8,7 @@ use App\Models\Image;
 use App\Services\ImageService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -96,6 +97,23 @@ class ImageController extends Controller
 
     public function destroy($id)
     {
-        //
+        // ハードデリート
+        $image = Image::findOrFail($id);
+        $filePath = "public/products/{$image->filename}";
+
+        // ストレージから削除
+        if (Storage::exists($filePath)) {
+            Storage::delete($filePath);
+        }
+
+        // DBから削除
+        Image::findOrFail($id)->delete();
+
+        return redirect()
+            ->route('owner.images.index')
+            ->with([
+                'message' => "{$image->filename} を削除しました。",
+                'status' => 'alert',
+            ]);
     }
 }
