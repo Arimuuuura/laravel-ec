9f9de22e9cecc47e7e220085d1cb260b08882cf8
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-13 23:29:24 +0900

    Eager loading で、n + 1 問題を解決

--- src/app/Http/Controllers/Owner/ProductController.php
+++ src/app/Http/Controllers/Owner/ProductController.php
@@ -28,9 +28,16 @@ class ProductController extends Controller
 
     public function index()
     {
-        $products = Owner::findOrFail(Auth::id())->shop->product;
+//        $products = Owner::findOrFail(Auth::id())->shop->product;
+        $ownerInfo = Owner::with('shop.product.imageFirst')->where('id', Auth::id())->get();
 
-        return view('owner.products.index', compact('products'));
+//        foreach ($ownerInfo as $owner) {
+//            foreach ($owner->shop->product as $product) {
+//                dd($product->imageFirst->filename);
+//            }
+//        }
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     public function create()
--- src/resources/views/owner/products/index.blade.php
+++ src/resources/views/owner/products/index.blade.php
@@ -14,17 +14,19 @@
                         <button onclick="location.href='{{ route('owner.products.create') }}'" class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">新規登録</button>
                     </div>
                     <div class="flex flex-wrap">
-                        @foreach($products as $product)
-                            <div class="w-1/4 p-2 md:p-4">
-                                <a href="{{ route('owner.products.edit', ['product' => $product->id]) }}">
-                                    <div class="border rounded-md p-2 md:p-4">
-                                        <x-thumbnail :filename="$product->imageFirst->filename" type="products" />
-{{--                                        <div class="text-gray-700">--}}
-{{--                                            {{ $product->name }}--}}
-{{--                                        </div>--}}
-                                    </div>
-                                </a>
-                            </div>
+                        @foreach ($ownerInfo as $owner)
+                            @foreach ($owner->shop->product as $product)
+                                <div class="w-1/4 p-2 md:p-4">
+                                    <a href="{{ route('owner.products.edit', ['product' => $product->id]) }}">
+                                        <div class="border rounded-md p-2 md:p-4">
+                                            <x-thumbnail :filename="$product->imageFirst->filename" type="products" />
+    {{--                                        <div class="text-gray-700">--}}
+    {{--                                            {{ $product->name }}--}}
+    {{--                                        </div>--}}
+                                        </div>
+                                    </a>
+                                </div>
+                            @endforeach
                         @endforeach
                     </div>
 {{--                    {{ $products->links() }}--}}
