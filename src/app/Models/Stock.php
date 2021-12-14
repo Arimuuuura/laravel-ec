4793d0b1f9b709fa819da606f90723b3a01f03ea
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-14 22:10:14 +0900

    create Stock model & seeder

---
+++ src/app/Models/Stock.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+
+    // 変更が頻繁に起こる table (トランザクションの頭文字で t_ )
+    protected $table = 't_stocks';
+}
