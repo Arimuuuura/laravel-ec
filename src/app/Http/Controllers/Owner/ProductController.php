dbeb2e7c34c0b8a0f5cdcfd96f4d435cbd1a257b
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-14 23:09:45 +0900

    create Product category

--- src/app/Http/Controllers/Owner/ProductController.php
+++ src/app/Http/Controllers/Owner/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use App\Models\Owner;
+use App\Models\PrimaryCategory;
 use App\Models\Product;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,21 +31,28 @@ class ProductController extends Controller
 
     public function index()
     {
-//        $products = Owner::findOrFail(Auth::id())->shop->product;
         $ownerInfo = Owner::with('shop.product.imageFirst')->where('id', Auth::id())->get();
-
-//        foreach ($ownerInfo as $owner) {
-//            foreach ($owner->shop->product as $product) {
-//                dd($product->imageFirst->filename);
-//            }
-//        }
 
         return view('owner.products.index', compact('ownerInfo'));
     }
 
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+            ->select('id', 'name')
+            ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+            ->select('id', 'title', 'filename')
+            ->orderBy('updated_at', 'desc')
+            ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+            ->get();
+
+        return view('owner.products.create',
+            compact('shops', 'images', 'categories')
+        );
     }
 
     public function store(Request $request)
