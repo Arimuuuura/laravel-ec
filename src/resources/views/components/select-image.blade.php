407384d73ac7a08182666ce46de365f16f9b08e5
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-15 23:32:38 +0900

    modal から image 登録

--- src/resources/views/components/select-image.blade.php
+++ src/resources/views/components/select-image.blade.php
@@ -1,27 +1,49 @@
-<div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+@php
+if ($name === 'image1') $modal = 'modal-1';
+if ($name === 'image2') $modal = 'modal-2';
+if ($name === 'image3') $modal = 'modal-3';
+if ($name === 'image4') $modal = 'modal-4';
+if ($name === 'image5') $modal = 'modal-5';
+@endphp
+<div class="modal micromodal-slide" id="{{$modal}}" aria-hidden="true">
     <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-        <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+        <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{$modal}}-title">
             <header class="modal__header">
-                <h2 class="modal__title" id="modal-1-title">
-                    Micromodal 🔥
+                <h2 class="text-xl text-gray-700" id="{{$modal}}-title">
+                    ファイルを選択してください
                 </h2>
                 <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
             </header>
-            <div class="modal__content" id="modal-1-content">
-                <p>This is a completely accessible modal.</p>
-                <p>
-                    Try hitting the <code>tab</code> key* and notice how the focus stays within the modal itself. To close modal
-                    hit the <code>esc</code> button, click on the overlay or just click the close button.
-                </p>
-                <p>
-                    <small>* <code>alt+tab</code> in safari</small>
-                </p>
+            <div class="modal__content" id="{{$modal}}-content">
+                <div class="flex flex-wrap">
+                    @foreach($images as $image)
+                        <div class="w-1/4 p-2 md:p-4">
+                            <div class="border rounded-md p-2 md:p-4">
+                                <img src="{{ asset("storage/products/{$image->filename}") }}"
+                                     class="image"
+                                     data-id='{{ "{$name}_{$image->id}" }}'
+                                     data-file="{{ $image->filename }}"
+                                     data-path="{{ asset('storage/products/') }}"
+                                     data-modal="{{ $modal }}"
+                                     alt="images">
+                                <div class="text-gray-700">
+                                    {{ $image->title }}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
             <footer class="modal__footer">
-                <button type="button" class="modal__btn modal__btn-primary">Continue</button>
-                <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
+                <button type="button" class="modal__btn" data-micromodal-close aria-label="Close">Close</button>
             </footer>
         </div>
     </div>
 </div>
-<a data-micromodal-trigger="modal-1" href="javascript:;">Open Modal Test</a>
+<div class="flex justify-around items-center mb-4">
+    <a data-micromodal-trigger="{{$modal}}" href="javascript:;">ファイルを選択</a>
+    <div class="w-1/4">
+        <img src="" alt="image" id="{{ $name }}_thumbnail">
+    </div>
+</div>
+<input type="hidden" id="{{ $name }}_hidden" name="{{ $name }}" value="">
