975775cd330ee2fd9421549f2b4343cec654a390
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-17 00:29:29 +0900

    create Product edit

--- src/resources/views/components/select-image.blade.php
+++ src/resources/views/components/select-image.blade.php
@@ -4,9 +4,11 @@ if ($name === 'image2') $modal = 'modal-2';
 if ($name === 'image3') $modal = 'modal-3';
 if ($name === 'image4') $modal = 'modal-4';
 if ($name === 'image5') $modal = 'modal-5';
+$c_Image = $currentImage ?? '';
+$c_Id = $currentId ?? '';
 @endphp
 <div class="modal micromodal-slide" id="{{$modal}}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
         <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{$modal}}-title">
             <header class="modal__header">
                 <h2 class="text-xl text-gray-700" id="{{$modal}}-title">
@@ -43,7 +45,7 @@ if ($name === 'image5') $modal = 'modal-5';
 <div class="flex justify-around items-center mb-4">
     <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{$modal}}" href="javascript:;">ファイルを選択</a>
     <div class="w-1/4">
-        <img src="" alt="image" id="{{ $name }}_thumbnail">
+        <img @if($c_Image) src='{{ asset("storage/products/{$c_Image}") }}' @else src="" @endif alt="image" id="{{ $name }}_thumbnail">
     </div>
 </div>
-<input type="hidden" id="{{ $name }}_hidden" name="{{ $name }}" value="">
+<input type="hidden" id="{{ $name }}_hidden" name="{{ $name }}" value="{{ $c_Id }}">
